098363c6f56ce3afdf3a79472080d5f198d3d155
Vinita Naik <100198999+VinitaNaik@users.noreply.github.com>
2025-02-05 10:26:26 +0530

    Add files via upload

---
+++ Candidate.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class candidateDetails {
+    int rollNo;
+    String name, remarks;
+    float score;
+
+
+    void enterData(){
+        Scanner S = new Scanner(System.in);
+        System.out.print("Enter Roll No.: ");
+        rollNo = S.nextInt();
+
+        System.out.print("Enter Name: ");
+        name = S.next();
+
+        System.out.print("Enter Score: ");
+        score = S.nextFloat();
+
+        System.out.println("");
+
+        assignRem();
+    }
+
+    void assignRem(){
+        if(score >= 50) {
+            remarks = "Selected";       
+        }
+        else {
+            remarks = "Not Selected";
+        }
+    }
+    
+
+    void displayData()  {
+        System.out.println("<<< CANDIDATE DETAILS >>>");
+        System.out.println("Roll No.: " + rollNo);
+        System.out.println("Name: " + name);
+        System.out.println("Score: " + score);
+        System.out.println("Remarks: " + remarks);
+    }
+}
+
+
+
+class Candidate {
+    public static void main(String args[]) {
+
+        candidateDetails obj = new candidateDetails();
+        obj.enterData();
+        obj.assignRem();
+        obj.displayData();
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;       //experiment 6
+
+class employeeDetails {
+    int empNo;
+    String empName;
+    double salary, hra, da, netPay;  //hra=house rent allowance, dr=dearness allowance
+
+    void haveData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Employee Number: ");
+        empNo = sc.nextInt();
+    
+        System.out.print("Enter Name: ");
+        empName = sc.next();
+
+        System.out.print("Enter Salary: ");
+        salary = sc.nextDouble();
+
+        System.out.print("Enter House Rent Allowance: ");
+        hra = sc.nextDouble();
+
+        System.out.print("Enter Dearness Allowance: ");
+        da = sc.nextDouble();
+
+        calculateNetPay();  //invoked calculation
+
+        System.out.println("");
+    }
+
+    void calculateNetPay() {
+        netPay = salary + hra + da;
+    }
+
+    void displayData() {
+        System.out.println("<<< EMPLOYEE DETAILS >>>");
+        System.out.println("Employee Number: " + empNo);
+        System.out.println("Employee Name: " + empName);
+        System.out.println("Employee Salary: " + salary);
+        System.out.println("Employee House Rent Allowance: " + hra);
+        System.out.println("Employee Dearness Allowance: " + da);
+        System.out.println("Employee Net Pay: " + netPay);
+    }
+}
+
+class Employee {
+    public static void main(String args[]) {
+        employeeDetails obj = new employeeDetails();
+        obj.haveData();
+        obj.displayData();
+    }
+}
+
+
+
+
